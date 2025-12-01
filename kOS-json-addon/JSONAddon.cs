c10afd55ea4b9d54fa827d88b1969275a8848437
Thr0in <xthroin@gmail.com>
2025-12-01 03:44:23 +0100

    Add JSONAddon for JSON parsing and stringifying
    Introduces a new kOS addon that provides STRINGIFY and PARSE suffixes for converting kOS structures to and from JSON strings using basic UnityEngine.JsonUtility.

---
+++ kOS-json-addon/JSONAddon.cs
@@ -0,0 +1,38 @@
+﻿using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using System;
+using UnityEngine;
+
+namespace kOS.AddOns.Json
+{
+    [kOSAddon("JSON")]
+    [kOS.Safe.Utilities.KOSNomenclature("JSONAddon")]
+    public class Addon : Suffixed.Addon
+    {
+        public Addon(SharedObjects shared) : base(shared)
+        {
+            InitializeSufixes();
+        }
+
+        public override BooleanValue Available()
+        {
+            return true;
+        }
+
+        private void InitializeSufixes()
+        {
+            AddSuffix("STRINGIFY", new OneArgsSuffix<StringValue, Structure>(Stringify, "Get a json string for an object"));
+            AddSuffix("PARSE", new OneArgsSuffix<Structure, StringValue>(Parse, "Get an object from a json string"));
+        }
+
+        private StringValue Stringify(Structure obj)
+        {
+            return UnityEngine.JsonUtility.ToJson(obj);
+        }
+
+        private Structure Parse(StringValue json)
+        {
+            return JsonUtility.FromJson<Structure>(json);
+        }
+    }
+}
