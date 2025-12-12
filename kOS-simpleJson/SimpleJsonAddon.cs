b742627b2bfd9af51f3f3cc61e851c8d1cfa2663
Thr0in <xthroin@gmail.com>
2025-12-12 19:05:53 +0100

    Add ISSTRINGIFIABLE suffix to SimpleJsonAddon
    Introduces the ISSTRINGIFIABLE suffix to check if a structure can be serialized to JSON. Also updates the ISPARSEABLE suffix description for clarity.

--- kOS-simpleJson/SimpleJsonAddon.cs
+++ kOS-simpleJson/SimpleJsonAddon.cs
@@ -1,4 +1,5 @@
-﻿using kOS.Safe.Encapsulation;
+﻿using kOS.Safe;
+using kOS.Safe.Encapsulation;
 using kOS.Safe.Encapsulation.Suffixes;
 using kOS.Safe.Exceptions;
 using kOS.Safe.Serialization;
@@ -24,10 +25,11 @@ namespace kOS.AddOns.Json
         private void InitializeSuffixes()
         {
             AddSuffix("STRINGIFY", new OneArgsSuffix<StringValue, Structure>(Stringify, "Get a json string for an object."));
+            AddSuffix("ISSTRINGIFIABLE", new OneArgsSuffix<BooleanValue, Structure>(IsStringifiable, "Returns true if a structure can be serialized, otherwise false."));
             AddSuffix("PARSE", new OneArgsSuffix<Structure, StringValue>(Parse, "Get an object from a json string."));
             AddSuffix("PARSEORELSE", new TwoArgsSuffix<Structure, StringValue, Structure>(ParseOrElse, "Get an object from a json string, or else return the fallback value."));
             AddSuffix("PARSEORELSEGET", new TwoArgsSuffix<Structure, StringValue, KOSDelegate>(ParseOrElseGet, "Get an object from a json string or else call a delegate and return its value."));
-            AddSuffix("ISPARSEABLE", new OneArgsSuffix<BooleanValue, StringValue>(IsParseable, "Returns true if the string can be parsed as json."));
+            AddSuffix("ISPARSEABLE", new OneArgsSuffix<BooleanValue, StringValue>(IsParseable, "Returns true if the string can be parsed as json, otherwise false."));
         }
 
         private StringValue Stringify(Structure obj)
@@ -36,6 +38,22 @@ namespace kOS.AddOns.Json
 
             string serializedString = new SafeSerializationMgr(shared).Serialize(serialized, SimpleJsonFormatter.WriterInstance, false);
             return new StringValue(serializedString);
+        }
+
+        private BooleanValue IsStringifiable(Structure obj)
+        {
+            SerializableStructure serialized = obj as SerializableStructure;
+            if (serialized == null) return new BooleanValue(false);
+
+            try
+            {
+                Dump dump = new SafeSerializationMgr(shared).Dump(serialized);
+                return SimpleJsonFormatter.WriterInstance.IsStringifiable(dump);
+            }
+            catch (Exception)
+                        {
+                return new BooleanValue(false);
+            }
         }
 
         private Structure Parse(StringValue json)
